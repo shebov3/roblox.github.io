0e3abe50010c2b2ae23369ce4d9ef5a0bf9c62fe
shebov3 <shehabtarik425@yahoo.com>
2024-05-29 11:50:42 +0300

    publish

--- src/ReplicatedStorage/Modules/Classes/Player/init.lua
+++ src/ReplicatedStorage/Modules/Classes/Player/init.lua
@@ -1,10 +1,23 @@
 local playerClass = {}
 playerClass.__index = playerClass
+playerClass.__tostring = function(self)
+    return `Money: {self.Money}`
+end
 
-function playerClass.new(player : Player)
-    local PlayerController = script.PlayerController:Clone()
-    PlayerController.Parent = player
-    require(PlayerController):init(playerClass)
+function playerClass.new(player: Player)
+	local controller = script.PlayerController:Clone()
+	controller.Parent = player
+
+	local playerController = require(controller)
+	playerController:init(playerClass)
+
+	playerController.Money = 100
+	playerController.Inventory = {}
+	playerController.Skills = {}
+end
+
+function playerClass:AddMoney(Money)
+	self.Money += Money
 end
 
 return playerClass
--- src/ServerScriptService/Framework/init.server.lua
+++ src/ServerScriptService/Framework/init.server.lua
@@ -1,7 +1,9 @@
-local replicatedStorage = game:GetService("ReplicatedStorage")
-local classes = replicatedStorage.Modules.classes
-local playerClass = require(classes.player)
+local classes = game.ReplicatedStorage.Modules.Classes
+local playerClass = require(classes.Player)
 
 game.Players.PlayerAdded:Connect(function(player)
 	playerClass.new(player)
+	local playerController = require(player.PlayerController)
+	playerController:AddMoney(50)
+	print(tostring(playerController))
 end)
