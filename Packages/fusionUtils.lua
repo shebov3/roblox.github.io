6ef94eb85bac94e553a50bbd5b91aa0ee91e46a5
shebov3 <shehabtarik425@yahoo.com>
2024-10-04 12:55:38 +0300

    last update

---
+++ Packages/fusionUtils.lua
@@ -0,0 +1,1 @@
+return require(script.Parent._Index["virtualbutfake_fusion-utils@0.1.2"]["fusion-utils"])
--- src/ReplicatedFirst/UI/Stories/Archive/TextLabel.lua
+++ src/ReplicatedFirst/UI/Stories/Archive/TextLabel.lua
@@ -5,8 +5,8 @@ local Fusion = require(Packages.Fusion)
 return function(props)
 	return Fusion.New("TextLabel")({
 		AnchorPoint = Vector2.new(0.5, 0.5),
-		Text = props.Text,
-		Position = props.Position,
+		Text = props.Text or "Test",
+		Position = props.Position or UDim2.new(0.5, 0, 0.5, 0),
 		TextSize = 24,
 		Font = Enum.Font.GothamMedium,
 	})
